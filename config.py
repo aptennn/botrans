6d034abd0d8cde93b67e7dfc9623caddb4150f38
cfd <egor2015-6@mail.ru>
2020-05-16 10:26:36 +0300

    choose lang

--- config.py
+++ config.py
@@ -1,1 +1,8 @@
 TOKEN = ''
+STARTMSG = "Hi, i am translator bot. I use google python and google API."
+LANGUES = ['ru', 'de', 'en']
+LANGDICT = {
+    'Russian': 'ru',
+    'Deutch': 'de',
+    'English': 'en'
+}
--- main.py
+++ main.py
@@ -5,14 +5,39 @@ from telebot import types
 
 bot = telebot.TeleBot(cfg.TOKEN)
 transl = Translator(0)
+lang = 'en'
+listlang = cfg.LANGUES
 
 
 # translator = Translator()
-# b = translator.translate('안녕하세요.', dest='ru', scr='')
+# b = translator.translate('안녕하세요.', dest='ru', scr='') # scr source lang , dest trans lang
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(message.from_user.id, cfg.STARTMSG)
+
+
+@bot.message_handler(commands=['chose'])
+def setl(message):
+    board = types.InlineKeyboardMarkup()  # keyborad
+
+    for i, j in cfg.LANGDICT.items():
+        langue = types.InlineKeyboardButton(text=j, callback_data=i)
+        board.add(langue)
+
+    bot.send_message(message.from_user.id, text='Choose lang', reply_markup=board)
+
+
+@bot.callback_query_handler(func=lambda call: True)  # keyboard
+def callback_worker(call):
+    global lang
+    if call.data in listlang:
+        lang = call.data
+        bot.send_message(call.message.chat.id, "You have choosen lang")
+
 
 @bot.message_handler(content_types=['text'])
 def text(message):
-    word = transl.translate(message.text).text
+    word = transl.translate(message.text, dest=lang).text
     bot.send_message(message.from_user.id, word)
 
 
